5e6ad54d5670c5df34b343bd72c589fbec66fe3e
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-02-20 17:32:57 +0900

    added basic routes

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail, presence: true
-  validates :ingredient, presence: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.delete_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+data = open(url).read
+ingredients_json = JSON.parse(data)
+
+ingredients_json["drinks"].each do |hash|
+  Ingredient.create!(name: hash["strIngredient1"])
+end
+
+# ingredients_json["drinks"][0]["strIngredient1"]
+
+# ingredients_json["drinks"][1]["strIngredient1"]
